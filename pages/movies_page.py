030485fad0830aae306bc474694d86ee4aab9e5e
Sascco <sascco2020@gmail.com>
2025-09-16 21:04:09 -0500

    workflow5

---
+++ pages/movies_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class MoviesPage(BasePage): #Localizadores de los elementos requeridos
+    pass
+
+
+    def __init__(self,driver):
+        
--- steps/peliculas.steps.py
+++ steps/peliculas.steps.py
@@ -1,1 +1,5 @@
-## hola mundo
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from pages.home_page import HomePage
+
+

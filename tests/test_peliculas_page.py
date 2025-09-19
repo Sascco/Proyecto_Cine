319c6ddd9b8e6575db4db06c4d5ec79157fddeef
Sascco <sascco2020@gmail.com>
2025-09-19 18:45:38 -0500

    """test.movies"""

---
+++ tests/test_peliculas_page.py
@@ -0,0 +1,13 @@
+import pytest
+from pages.peliculas_page import PeliculasPage
+
+def test_abrir_cartelera(driver):
+    page = PeliculasPage(driver)
+    page.abrir()
+
+    assert "fake-cinema" in driver.current_url
+    print(f"Estas en la pagina: {driver.current_url}")
+
+    titulo = page.obtener_titulo_cartelera()
+    assert titulo == "Cartelera", f"Título esperado: 'Cartelera', pero se encontró: '{titulo}'"
+    print(f"Título de cartelera encontrado: {titulo}")

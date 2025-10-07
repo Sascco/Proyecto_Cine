7b46b99eb0a020408aacb44f89b35e9aad5c38d7
Roberto Gamboa <robertogamboa.9006@gmail.com>
2025-10-06 20:47:57 -0600

    se agrega promos.feature y promos_pages.py

---
+++ features/promos.feature
@@ -0,0 +1,23 @@
+Feature: Ver y seleccionar PROMOS
+Como usuario
+Quiero poder visualizar y seleccionar promociones disponibles
+Para aprovechar descuentos y combos especiales en mis compras
+
+Background:
+  Given que estoy en la página Promos
+
+Scenario: Ver lista de promociones disponibles
+  When la página carga completamente
+  Then debo ver una lista de promociones disponibles
+  And cada promoción debe mostrar su título, descripción y precio
+  And debo ver en el listado las siguientes promociones:
+    | nombre                  |
+    | Combo Pareja       	    |
+    | Miércoles 2x1        	|
+    | Combo Kids           	|
+    | Tarjeta Membresía	    |
+    | Descuento Matiné 	    |
+    | Combo Familiar     	    |
+    | Precio Estudiantes	    |
+    | Palomitas Refill		|
+    | Bebida Grande Gratis    |
---
+++ pages/promos_page.py
@@ -0,0 +1,48 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.helpers import PromosLocators
+
+
+
+
+class PromosPage:
+    url = "https://fake-cinema.vercel.app/promos"   # Pagina web promos
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def abrir(self):
+        self.driver.get(self.url)
+
+    def obtener_titulo_promociones(self):
+        return WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located(PromosLocators.TITLE_PROMOS)
+        ).text
+
+    def obtener_promociones(self):
+        return WebDriverWait(self.driver, 5).until(
+            EC.presence_of_all_elements_located(PromosLocators.PROMO_ITEMS)
+        )
+
+    def obtener_nombre_promo(self, index):
+        promos = self.obtener_promociones()
+        return promos[index].find_element(*PromosLocators.PROMO_NAME).text
+
+    def obtener_descripcion_promo(self, index):
+        promos = self.obtener_promociones()
+        return promos[index].find_element(*PromosLocators.PROMO_DESCRIPTION).text
+
+    def obtener_precio_promo(self, index):
+        promos = self.obtener_promociones()
+        return promos[index].find_element(*PromosLocators.PROMO_PRICE).text
+
+    def obtener_todas_las_promos(self):
+        promos = self.obtener_promociones()
+        resultado = []
+        for promo in promos:
+            resultado.append({
+                "nombre": promo.find_element(*PromosLocators.PROMO_NAME).text,
+                "descripcion": promo.find_element(*PromosLocators.PROMO_DESCRIPTION).text,
+                "precio": promo.find_element(*PromosLocators.PROMO_PRICE).text
+            })
+        return resultado
